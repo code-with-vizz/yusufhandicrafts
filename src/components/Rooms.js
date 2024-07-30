64bb2bc6c8b7091ce19a37b2f89c72d7e4ee40e4
vizz <140245450+code-with-vizz@users.noreply.github.com>
2024-07-30 15:20:41 +0530

    Update Rooms.js

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import room from './assets/room.mp4';
+import room from './assets/yusuf.mp4';
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/pagination';
