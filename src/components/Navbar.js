113fba6aa6c8aa371f2d7d8fe9a02331eb2d0515
vizz <140245450+code-with-vizz@users.noreply.github.com>
2024-07-30 17:16:09 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     <nav className="flex items-center justify-between p-4 shadow-md px-4 md:px-14">
       <div className="flex items-center space-x-6">
         <Link  to="/rooms">
-        <img  src={logo} alt="Logo" className="h-10 w-auto lg:w-[80px] cursor-pointer "/>
+        <img  src={logo} alt="Logo" className="h-10 w-auto lg:w-[100px] cursor-pointer "/>
        </Link> {/* Responsive logo */}
       </div>
       <div className="flex items-center space-x-6 text-zinc-800 md:flex md:space-x-6">
